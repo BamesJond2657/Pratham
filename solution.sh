97f6a2449dfff2e143938fd9ad21136d001b3121
BamesJond2657 <am25b027@smail.iitm.ac.in>
2025-08-26 23:22:16 +0530

    Lab Assignment-3 DA1300

---
+++ solution.sh
@@ -0,0 +1,12 @@
+sort -n numbers.txt
+ip addr show
+cat readme.txt
+wc -l readme.txt
+grep -ilr "error" logs/
+tail -10 app.log
+chmod a+x script.sh
+grep "TODO" *.py
+history 20
+ps aux --sort=-%mem
+sudo find / -type d -name backup
+sed 's/foo/bar/g' example.txt > new_example.txt
