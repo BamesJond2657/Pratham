b2779a1a10d7a4c61e169d2f46a94087b06cd3b9
BamesJond2657 <am25b027@smail.iitm.ac.in>
2025-08-30 03:59:04 +0530

    Minor Changes

--- solution.sh
+++ solution.sh
@@ -1,12 +1,12 @@
 sort -n numbers.txt
 ip addr show
 cat readme.txt
-wc -l readme.txt
+wc -l data.csv
 grep -ilr "error" logs/
 tail -10 app.log
 chmod a+x script.sh
 grep "TODO" *.py
 history 20
 ps aux --sort=-%mem
-sudo find / -type d -name backup
+find / -type d -name backup
 sed 's/foo/bar/g' example.txt > new_example.txt
